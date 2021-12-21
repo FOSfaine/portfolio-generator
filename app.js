fc4f9ea861503c5b6f99b0c709e9c7d50de50fae
Esther Cho <eunsem.64642@gmail.com>
2021-12-21 15:50:09 -0700

    added inquirer prompts to app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ]);
+};
+
+promptUser().then(answers => console.log(answers));
