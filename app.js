5f009348320848309079bda3157f329e269f2bb2
Esther Cho <eunsem.64642@gmail.com>
2021-12-29 22:36:33 -0700

    css file saved

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
-=======
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 const { writeFile, copyFile } = require('./utils/generate-site');
@@ -158,6 +144,5 @@ promptUser()
   .catch(err => {
     console.log(err);
   });
->>>>>>> develop
 
 
